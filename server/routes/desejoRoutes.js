620a0c2c366c35ccd95cfa527e866bd47773f4fd
PedroHMFreire <pedrohfreire@gmail.com>
2025-06-04 17:41:59 -0300

    Update desejoRoutes.js

--- server/routes/desejoRoutes.js
+++ server/routes/desejoRoutes.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const desejoController = require('../controllers/desejoController');
 
+router.get('/produtos', desejoController.getProdutos);
+router.get('/desejos', desejoController.getDesejos);
+router.post('/desejos', desejoController.createDesejo);
+
+module.exports = router;
