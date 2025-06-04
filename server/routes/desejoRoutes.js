a4e3a64636ce487f605b7786b4d5ad28010a3427
PedroHMFreire <pedrohfreire@gmail.com>
2025-06-04 18:01:23 -0300

    Delete server directory

--- server/routes/desejoRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const desejoController = require('../controllers/desejoController');
-
-router.get('/produtos', desejoController.getProdutos);
-router.get('/desejos', desejoController.getDesejos);
-router.post('/desejos', desejoController.createDesejo);
-
-module.exports = router;
