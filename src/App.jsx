e09c845855dc15a9b89a972ff1b5171bb544242d
PedroHMFreire <pedrohfreire@gmail.com>
2025-06-05 09:52:02 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+/* src/App.jsx */
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Desejos from './components/Desejos'
@@ -6,15 +8,27 @@ import Ranking from './components/Ranking'
 import Cadastros from './components/Cadastros'
 
 function App() {
+  const [desejos, setDesejos] = useState([])
+  const [vendedores, setVendedores] = useState([])
+  const [lojas, setLojas] = useState([])
+  const [categorias, setCategorias] = useState([])
+
+  useEffect(() => {
+    setDesejos(JSON.parse(localStorage.getItem('desejos')) || [])
+    setVendedores(JSON.parse(localStorage.getItem('vendedores')) || [])
+    setLojas(JSON.parse(localStorage.getItem('lojas')) || [])
+    setCategorias(JSON.parse(localStorage.getItem('categorias')) || [])
+  }, [])
+
   return (
     <Router>
       <Navbar />
       <div className="p-4">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/desejos" element={<Desejos />} />
-          <Route path="/ranking" element={<Ranking />} />
-          <Route path="/cadastros" element={<Cadastros />} />
+          <Route path="/" element={<Home desejos={desejos} setDesejos={setDesejos} vendedores={vendedores} lojas={lojas} categorias={categorias} />} />
+          <Route path="/desejos" element={<Desejos desejos={desejos} setDesejos={setDesejos} />} />
+          <Route path="/ranking" element={<Ranking desejos={desejos} />} />
+          <Route path="/cadastros" element={<Cadastros vendedores={vendedores} setVendedores={setVendedores} lojas={lojas} setLojas={setLojas} categorias={categorias} setCategorias={setCategorias} />} />
         </Routes>
       </div>
     </Router>
